5015f69bb75a763d8b4d0ab802ae10ad42f0a648
Aali <mr7aali@gmail.com>
2025-04-18 17:56:13 +0600

    banner done

--- src/components/layout/HomeSlider.tsx
+++ src/components/layout/HomeSlider.tsx
@@ -1,59 +1,132 @@
-import { Button, Image, Link } from '@nextui-org/react'
-import SlideBackground from './SlideBackground'
-import { SectionProps } from '@/types/SectionProps'
+import { Button, Image, Link } from "@nextui-org/react";
+import SlideBackground from "./SlideBackground";
+import { SectionProps } from "@/types/SectionProps";
 
 const HomeSlider = ({ className }: SectionProps) => {
   return (
     <section className={className}>
-      <div data-hs-carousel='{"loadingClasses": "opacity-0", "isAutoPlay": true}' className="relative h-[850px] z-0">
+      <div
+        data-hs-carousel='{"loadingClasses": "opacity-0", "isAutoPlay": true}'
+        className="relative h-96 sm:h-[630px]  lg:h-[850px] z-0"
+      >
         <div className="hs-carousel relative overflow-hidden w-full h-full">
           <div className="hs-carousel-body w-full absolute top-0 bottom-0 start-0 flex flex-nowrap duration-700 ease-in-out delay-200 opacity-0">
-
             {/*First Slide*/}
-            <SlideBackground bgImage={'/assets/slider_bg_1.jpg'}>
+            <SlideBackground bgImage={"/assets/slider_bg_1.jpg"}>
               <div className="w-full flex flex-col justify-center text-center h-full absolute z-10">
-                <span className="font-nothingYouCouldDo text-primary text-[40px] mb-4">Welcome</span>
-                <h1 className="mb-8 text-[40px]">We cooked your desired <span className='block'>Pizza Recipe</span></h1>
-                <h3 className="mb-16">A small river named Duden flows by their place and supplies it with the <span className='block'>necessary regelialia.</span></h3>
+                <span className="font-nothingYouCouldDo text-primary text-[40px] mb-4">
+                  Welcome
+                </span>
+                <h1 className="mb-4 text-xl sm:text-[40px] ">
+                  We cooked your desired
+                </h1>
+                <p className="mb-2 sm:mb-8 text-xl sm:text-[40px]">
+                  Pizza Recipe
+                </p>
+                <h3 className="mb-16 text-sm sm:text-xl">
+                  A small river named Duden flows by their place and supplies it
+                  with the <span className="block">necessary regelialia.</span>
+                </h3>
                 <p>
-                  <Button as={Link} href='/menu' color='primary' radius='none' size='lg' className='py-4 px-6 text-dark'>Order Now</Button>
-                  <Button as={Link} href='/menu' radius='none' size='lg' className='bg-transparent border-2 py-4 px-6 ml-2'>View Menu</Button>
+                  <Button
+                    as={Link}
+                    href="/menu"
+                    color="primary"
+                    radius="none"
+                    size="lg"
+                    className="py-4 px-6 text-dark"
+                  >
+                    Order Now
+                  </Button>
+                  <Button
+                    as={Link}
+                    href="/menu"
+                    radius="none"
+                    size="lg"
+                    className="bg-transparent border-2 py-4 px-6 ml-2"
+                  >
+                    View Menu
+                  </Button>
                 </p>
               </div>
             </SlideBackground>
 
             {/*Second Slide*/}
-            <SlideBackground bgImage={'/assets/slider_bg_2.jpg'}>
+            <SlideBackground bgImage={"/assets/slider_bg_2.jpg"}>
               <div className="w-full flex flex-row gap-6 justify-center items-center h-full absolute z-10">
-                <div className='w-1/4'>
-                  <Image src="/assets/slider_pizza_1.png" alt=""/>
+                <div className="hidden sm:flex  w-1/4">
+                  <Image src="/assets/slider_pizza_1.png" alt="" />
                 </div>
-                <div className='max-w-xl ml-6'>
-                  <span className="font-nothingYouCouldDo text-primary text-[40px] mb-4">Crunchy</span>
+                <div className="max-w-xl ml-6">
+                  <span className="font-nothingYouCouldDo text-primary text-[40px] mb-4">
+                    Crunchy
+                  </span>
                   <h1 className="mb-8 text-[40px]">Italian Pizza</h1>
-                  <h3 className="mb-16">A small river named Duden flows by their place and supplies it with the necessary regelialia.</h3>
+                  <h3 className="mb-16">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary regelialia.
+                  </h3>
                   <p>
-                    <Button as={Link} href='/menu' color='primary' radius='none' size='lg' className='py-4 px-6 text-dark'>Order Now</Button>
-                    <Button as={Link} href='/menu' radius='none' size='lg' className='bg-transparent border-2 py-4 px-6 ml-2'>View Menu</Button>
+                    <Button
+                      as={Link}
+                      href="/menu"
+                      color="primary"
+                      radius="none"
+                      size="lg"
+                      className="py-4 px-6 text-dark"
+                    >
+                      Order Now
+                    </Button>
+                    <Button
+                      as={Link}
+                      href="/menu"
+                      radius="none"
+                      size="lg"
+                      className="bg-transparent border-2 py-4 px-6 ml-2"
+                    >
+                      View Menu
+                    </Button>
                   </p>
                 </div>
               </div>
             </SlideBackground>
 
             {/*Third Slide*/}
-            <SlideBackground bgImage={'/assets/slider_bg_2.jpg'}>
+            <SlideBackground bgImage={"/assets/slider_bg_2.jpg"}>
               <div className="w-full flex flex-row gap-6 justify-center items-center h-full absolute z-10">
-                <div className='max-w-xl text-end mr-6'>
-                  <span className="font-nothingYouCouldDo text-primary text-[40px] mb-4">Delicious</span>
+                <div className="max-w-xl text-end mr-6">
+                  <span className="font-nothingYouCouldDo text-primary text-[40px] mb-4">
+                    Delicious
+                  </span>
                   <h1 className="mb-8 text-[40px]">Italian Cuizine</h1>
-                  <h3 className="mb-16">A small river named Duden flows by their place and supplies it with the necessary regelialia.</h3>
+                  <h3 className="mb-16">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary regelialia.
+                  </h3>
                   <p>
-                    <Button as={Link} href='/menu' color='primary' radius='none' size='lg' className='py-4 px-6 text-dark'>Order Now</Button>
-                    <Button as={Link} href='/menu' radius='none' size='lg' className='bg-transparent border-2 py-4 px-6 ml-2'>View Menu</Button>
+                    <Button
+                      as={Link}
+                      href="/menu"
+                      color="primary"
+                      radius="none"
+                      size="lg"
+                      className="py-4 px-6 text-dark"
+                    >
+                      Order Now
+                    </Button>
+                    <Button
+                      as={Link}
+                      href="/menu"
+                      radius="none"
+                      size="lg"
+                      className="bg-transparent border-2 py-4 px-6 ml-2"
+                    >
+                      View Menu
+                    </Button>
                   </p>
                 </div>
-                <div className='w-1/4'>
-                  <Image src="/assets/slider_pizza_2.png" alt=""/>
+                <div className="hidden sm:flex w-1/4">
+                  <Image src="/assets/slider_pizza_2.png" alt="" />
                 </div>
               </div>
             </SlideBackground>
@@ -61,7 +134,10 @@ const HomeSlider = ({ className }: SectionProps) => {
         </div>
 
         {/*Indicator buttons*/}
-        <div className="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-3">
+        <div
+          data-hs-carousel-indicators
+          className="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-3"
+        >
           <span className="hs-carousel-active:bg-white hs-carousel-active:border-none w-[18px] h-[18px] border-2 border-gray-400 rounded-full cursor-pointer flex items-center justify-center">
             <span className="hs-carousel-active:bg-white hs-carousel-active:border-gray-500 w-3 h-3 border border-gray-400 rounded-full cursor-pointer "></span>
           </span>
@@ -74,7 +150,7 @@ const HomeSlider = ({ className }: SectionProps) => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HomeSlider
+export default HomeSlider;
