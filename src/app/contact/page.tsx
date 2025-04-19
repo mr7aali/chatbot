6d2e40330def5d407bc931e2f1054f66de526151
Aali <mr7aali@gmail.com>
2025-04-19 16:17:08 +0600

    d

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,23 +1,33 @@
-import ContactUsForm from '@/components/common/form/ContactUsForm'
-import React from 'react'
+import ContactUsForm from "@/components/common/form/ContactUsForm";
+import React from "react";
 
 const ContactPage = () => {
   return (
-    <div className='py-20 container'>
-      <div className="grid grid-cols-2">
+    <div className="py-20 container">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
         <div className="container flex flex-col gap-8">
-          <h1 className='mb-4'>Contact Information</h1>
-          <p >Address: <span className='text-primary'>Mirpur-13 Dhaka,Bangladesh</span></p>
-          <p>Phone: <span className='text-primary'>01967519057</span></p>
-          <p>Email: <span className='text-primary'>info@pizzafiesta.com</span></p>
-          <p>Business Hours: <span className='text-primary'>Monday-Friday 8:00am - 9:00pm</span></p>
+          <h1 className="mb-4">Contact Information</h1>
+          <p>
+            Address:{" "}
+            <span className="text-primary">Mirpur-13 Dhaka,Bangladesh</span>
+          </p>
+          <p>
+            Phone: <span className="text-primary">01967519057</span>
+          </p>
+          <p>
+            Email: <span className="text-primary">info@pizzafiesta.com</span>
+          </p>
+          <p>
+            Business Hours:{" "}
+            <span className="text-primary">Monday-Friday 8:00am - 9:00pm</span>
+          </p>
         </div>
-        <div className='container'>
+        <div className="container pt-20 lg:pt-0">
           <ContactUsForm />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
--- src/components/features/cart/CartProduct.tsx
+++ src/components/features/cart/CartProduct.tsx
@@ -9,24 +9,25 @@ interface CartProductProps {
 }
 
 const CartProduct = ({ product, productPrice, onRemove }: CartProductProps) => {
+  console.log(product.menuItem.image);
   return (
-    <div className="grid grid-cols-8 gap-4 border-b pt-2">
-      <div className="col-span-2">
+    <div className="grid grid-cols-1 sm:grid-cols-8 gap-4 border-b pt-2">
+      <div className="sm:col-span-2 col-span-1">
         <div
           style={{
-            backgroundImage: `url(Tk {product.menuItem.image})`,
+            backgroundImage: `url(${product.menuItem.image})`,
             borderRadius: "10%",
           }}
           className="bg-cover bg-center bg-no-repeat mb-4 w-[120px] h-[120px]"
         ></div>
       </div>
-      <div className="col-span-3 px-4">
+      <div className="sm:col-span-3 px-4 col-span-1">
         <p className="font-semibold">{product.menuItem.name}</p>
         {product.selectedSize && (
           <div className="text-sm text-gray-300 py-1">
             Size:{" "}
             <span>
-              {product.selectedSize.name} + Tk 
+              {product.selectedSize.name} + Tk
               {(product.selectedSize.price as number).toFixed(2)}
             </span>
           </div>
@@ -41,14 +42,16 @@ const CartProduct = ({ product, productPrice, onRemove }: CartProductProps) => {
           </div>
         )}
       </div>
-      <div className="items-start text-center">
+      <div className="text-left pl-4 sm:pl-4 sm:text-center">
         <p className="font-semibold">Quantity</p>
         <p>1</p>
       </div>
-      <div className="text-right font-semibold">Tk {productPrice.toFixed(2)}</div>
+      <div className="text-left pl-4 sm:pl-0 sm:text-right font-semibold">
+        Tk {productPrice.toFixed(2)}
+      </div>
       {!!onRemove && (
         <Tooltip content="Remove">
-          <div className="ml-6 cursor-pointer" onClick={onRemove}>
+          <div className="ml-6 cursor-pointer pb-4 sm:pb-0" onClick={onRemove}>
             <TrashIcon className={"w-6"} />
           </div>
         </Tooltip>
