93d0c40b67de1d436bb9debfbe85d34797fb9f02
Aali <mr7aali@gmail.com>
2025-07-07 20:36:27 +0600

    update

--- src/components/layout/HomeMenuItemCard.tsx
+++ src/components/layout/HomeMenuItemCard.tsx
@@ -8,8 +8,48 @@ interface HomeMenuItemCardProps {
   index: number;
 }
 
-const HomeMenuItemCard = ({ menuItem, index }: HomeMenuItemCardProps) => {
+const TextCard = ({
+  menuItem,
+  hasSizesOrExtras,
+  margin,
+}: {
+  menuItem: MenuItem;
+  margin: "top" | "bottom";
+  hasSizesOrExtras: boolean;
+}) => {
   const { data: session } = useSession();
+  return (
+    <div
+      className={`${
+        margin === "top" ? "mt-2" : margin === "bottom" ? "mb-2" : ""
+      } flex items-center backdrop-blur-sm bg-white/10  ml-2 mr-2  shadow-lg hover:shadow-xl transition-shadow duration-300 `}
+    >
+      <div className="flex flex-col gap-4 p-6 text-end">
+        <h3 className="text-xl font-semibold text-primary">{menuItem.name}</h3>
+        <p className="text-gray-400 line-clamp-3 text-sm">
+          {menuItem.description}
+        </p>
+        <div className="flex flex-col 2xl:flex-row gap-4 justify-end items-center">
+          <p className="text-primary font-medium">
+            {hasSizesOrExtras && <span className="text-gray-400">From: </span>}
+            Tk {(menuItem.basePrice as number).toFixed(2)}
+          </p>
+          <Button
+            as={Link}
+            href={session ? "/menu" : "/login"}
+            radius="sm"
+            size="sm"
+            className="bg-gradient-to-r from-primary to-primary/80 text-dark border border-primary/50 hover:from-primary/80 hover:to-primary hover:scale-105 transition-all duration-300 ease-in-out"
+          >
+            Order Now
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const HomeMenuItemCard = ({ menuItem, index }: HomeMenuItemCardProps) => {
   const hasSizesOrExtras =
     menuItem.sizes.length > 0 || menuItem.extraIngredientsPrices.length > 0;
 
@@ -20,65 +60,22 @@ const HomeMenuItemCard = ({ menuItem, index }: HomeMenuItemCardProps) => {
           style={{ backgroundImage: `url(${menuItem.image})` }}
           className="bg-cover bg-center bg-no-repeat"
         ></div>
-        <div
-          className="flex items-center"
-          style={{
-            backgroundImage:
-              "linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3))",
-          }}
-        >
-          <div className="flex flex-col gap-4 p-10">
-            <h3>{menuItem.name}</h3>
-            <p className="text-gray-400 line-clamp-3">{menuItem.description}</p>
-            <div className="flex  flex-col 2xl:flex-row gap-4">
-              <p className="text-primary">
-                {hasSizesOrExtras && <span>From: </span>}
-                Tk. {(menuItem.basePrice as number).toFixed(2)}
-              </p>
-              <Button
-                as={Link}
-                href={session ? "/menu" : "/login"}
-                radius="none"
-                size="sm"
-                className="bg-transparent border hover:bg-primary hover:text-dark"
-              >
-                Order
-              </Button>
-            </div>
-          </div>
-        </div>
+
+        <TextCard
+          menuItem={menuItem}
+          hasSizesOrExtras={hasSizesOrExtras}
+          margin="bottom"
+        />
       </div>
     );
   } else {
     return (
       <div className="grid grid-cols-2 ">
-        <div
-          className="flex items-center"
-          style={{
-            backgroundImage:
-              "linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3))",
-          }}
-        >
-          <div className="flex flex-col gap-4 p-10 text-end ">
-            <h3>{menuItem.name}</h3>
-            <p className="text-gray-400 line-clamp-3">{menuItem.description}</p>
-            <div className="flex flex-col 2xl:flex-row gap-4 justify-end">
-              <p className="text-primary">
-                {hasSizesOrExtras && <span>From: </span>}
-                Tk {(menuItem.basePrice as number).toFixed(2)}
-              </p>
-              <Button
-                as={Link}
-                href={session ? "/menu" : "/login"}
-                radius="none"
-                size="sm"
-                className="bg-transparent border hover:bg-primary hover:text-dark"
-              >
-                Order
-              </Button>
-            </div>
-          </div>
-        </div>
+        <TextCard
+          menuItem={menuItem}
+          hasSizesOrExtras={hasSizesOrExtras}
+          margin="top"
+        />
         <div
           style={{ backgroundImage: `url(${menuItem.image})` }}
           className="bg-cover bg-center bg-no-repeat"
